8e0ef99141b528efea677155b2638693866f7f9a
xTanEusores <44177748+xTanEusores@users.noreply.github.com>
2024-08-13 20:30:12 +0800

    Create script.js

---
+++ Practices/script.js
@@ -0,0 +1,20 @@
+// Regular Function
+function dance() {
+  console.log("Dancing!");
+}
+
+// Regular Arrow Function
+const danceArrow1 = () => {
+  console.log("Dancing Using Regular Arrow Function!")
+}
+// Shorthand Arrow Function
+const danceArrow2 = ()=> console.log("Dancing Using Arrow Shorthand Function!")
+
+//calling Regular Function
+dance()
+
+// calling regular arrow function
+danceArrow1()
+
+ // calling danceArrow function
+danceArrow2()
